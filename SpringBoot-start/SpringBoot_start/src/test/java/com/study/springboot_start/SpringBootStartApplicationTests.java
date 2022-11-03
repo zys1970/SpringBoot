60e3dba182c4a54c46017026a2adfb4f04923484
zys1970 <1970215988@qq.com>
2022-11-03 20:47:10 +0800

    SpringBoot_start

---
+++ SpringBoot-start/SpringBoot_start/src/test/java/com/study/springboot_start/SpringBootStartApplicationTests.java
@@ -0,0 +1,15 @@
+package com.study.springboot_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStartApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
