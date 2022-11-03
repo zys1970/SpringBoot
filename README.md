e7d2fe26bb3e0a94a833c9a66e52a712511d18d3
zys1970 <1970215988@qq.com>
2022-11-03 20:54:06 +0800

    jdbc

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 springboot学习记录
 
 1. SpringBoot_start 创建一个SpringBoot项目 [Spring Initializr](https://start.spring.io/)
-2. 
+2. SpringBoot_jdbc SpringBoot使用JDBC连接数据库
+3. 
---
+++ SpringBoot_jdbc/src/test/java/com/study/springboot_jdbc/SpringBootJdbcApplicationTests.java
@@ -0,0 +1,15 @@
+package com.study.springboot_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
